4d6b3a5efac970fc1edea5b7785d4cfd085a615c
Victor <varc.89@hotmail.com>
2023-03-22 21:30:39 -0300

    Agregando DELETE route

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -23,3 +23,13 @@ export const createEmployees = async (req, res) => {
         salary,
     })
 }
+
+export const deleteEmployee = async (req, res) => {
+    const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
+
+    if (result.affectedRows <= 0) return res.status(404).json({
+        message: 'Employee not found'
+    })
+
+    res.sendStatus(204)
+}
--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getEmployees, getEmployee, createEmployees } from "../controllers/employees.controller.js";
+import { getEmployees, getEmployee, createEmployees, deleteEmployee } from "../controllers/employees.controller.js";
 
 const router = Router();
 
 router.get('/employees', getEmployees);
 router.get('/employees/:id', getEmployee);
 router.post('/employees', createEmployees);
+router.delete('/employees/:id', deleteEmployee);
 
 export default router;
